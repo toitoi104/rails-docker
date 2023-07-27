f044a0873cd78b9e5f3329236f50825155f4b65a
shimizu toshinori <>
2023-07-27 23:24:15 +0900

    ログイン、ログアウト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def create
+    board_user = BoardUser.find_by(name: params[:name])
+
+    if board_user && board_user.authenticate(params[:password])
+      session[:user_id] = board_user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
